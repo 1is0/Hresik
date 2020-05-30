5b7b5f29cccb0f481a3d49e4ff784441c9577f82
black4ire <64589604+black4ire@users.noreply.github.com>
2020-05-30 23:06:47 +0300

    Add files via upload

---
+++ C#/laba 3/Figura.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+
+namespace geometricFigure
+{
+    class Figura
+    {
+        double a, b, c, d;
+
+        public Figura(double a)
+        {
+            this.a = a;
+        }
+        public Figura(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+
+        }
+        public Figura(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public Figura(double a, double b, double c, double d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+
+        public void Proverka(double a) // проверка
+        {
+            bool p = false;
+            if (this.a > 0 )
+            {
+                p = true;
+            }
+            if (p != true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Это не фигура");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+
+        public void Proverka(double a, double b) // проверка
+        {
+            bool p = false;
+            if (this.a > 0 && this.b > 0 )
+            {
+                p = true;
+            }
+            if (p != true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Это не фигура");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+
+        public void Proverka(double a, double b, double c) // проверка
+        {
+            bool p = false;
+            if (this.a > 0 && this.b > 0 && this.c > 0)
+            {
+                p = true;
+            }
+            if (p != true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Это не фигура");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+
+        public void Proverka(double a, double b, double c, double d) // проверка
+        {
+            bool p = false;
+            if (this.a > 0 && this.b > 0 && this.c > 0 && this.d > 0)
+            {
+                p = true;
+            }
+            if (p != true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Это не фигура");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+
+        public double Perimetr(double a)
+        {
+            return a + a + a + a;
+        }
+
+        public double Perimetr(double a, double b)
+        {
+            return a + a + b + b;
+        }
+
+        public double Perimetr(double a, double b, double c)
+        {
+            return a + b + c ;
+        }
+
+        public double Perimetr(double a, double b, double c, double d)
+        {
+            return a + b + c + d;
+        }
+    }
+}
---
+++ C#/laba 3/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace geometricFigure
+{
+    class Program
+    {
+        static double InputCheck()
+        {
+            double a;
+            while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка ввода!");
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Выберите фигуру:\n0 - треугольник\n1 - квадрат\n2 - прямоугольник\n3 - черырехугольник");
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка ввода!");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            Console.Clear();
+            switch (choice)
+            {
+                case 0:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("Здравствуйте введите данные о фигуре: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Первая сторона: ");
+                        double firstSide = InputCheck();
+                        Console.WriteLine("Вторая сторона: ");
+                        double secondSide = InputCheck();
+                        Console.WriteLine("Третья сторона: ");
+                        double thirdSide = InputCheck();
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Figura figura = new Figura(firstSide, secondSide, thirdSide);
+                        figura.Proverka(firstSide, secondSide, thirdSide);
+                        Console.WriteLine("Периметр треугольника: {0:#.##}", figura.Perimetr(firstSide, secondSide, thirdSide));
+                        Console.WriteLine();
+                        break;
+                    } 
+                case 1:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("Здравствуйте введите данные о фигуре: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Сторона: ");
+                        double firstSide = InputCheck();
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Figura figura = new Figura(firstSide);
+                        figura.Proverka(firstSide);
+                        Console.WriteLine("Периметр квадрата: {0:#.##}", figura.Perimetr(firstSide));
+                        Console.WriteLine();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("Здравствуйте введите данные о фигуре: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Первая/третья сторона: ");
+                        double firstSide = InputCheck();
+                        Console.WriteLine("Вторая/четвёртая сторона: ");
+                        double secondSide = InputCheck();
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Figura figura = new Figura(firstSide, secondSide);
+                        figura.Proverka(firstSide, secondSide);
+                        Console.WriteLine("Периметр прямоугольника: {0:#.##}", figura.Perimetr(firstSide, secondSide));
+                        Console.WriteLine();
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("Здравствуйте введите данные о фигуре: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Первая сторона: ");
+                        double firstSide = InputCheck();
+                        Console.WriteLine("Вторая сторона: ");
+                        double secondSide = InputCheck();
+                        Console.WriteLine("Третья сторона: ");
+                        double thirdSide = InputCheck();
+                        Console.WriteLine("Четвёртая сторона: ");
+                        double fourthSide = InputCheck();
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Figura figura = new Figura(firstSide, secondSide,thirdSide, fourthSide);
+                        figura.Proverka(firstSide, secondSide, thirdSide, fourthSide);
+                        Console.WriteLine("Периметр четырёхугольник: {0:#.##}", figura.Perimetr(firstSide, secondSide, thirdSide, fourthSide));
+                        Console.WriteLine();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ошибка!");
+                        break;
+                    }
+            }
+            Console.ReadLine();
+        }
+    }
+}
